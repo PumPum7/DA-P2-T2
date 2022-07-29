37e7b7fca1a223dad12e8a098bf2d32f4a0d8870
Michael <28806828+PumPum7@users.noreply.github.com>
2022-07-29 21:52:40 +0200

    task 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const init = 1;
-    setCount(init + count);
-  });
+	useEffect(() => {
+		const init = 1;
+		setCount(init + count);
+	}, []);
 
-  const increaseScore = () => setCount(1);
+	const increaseScore = () => setCount(previous => previous + 1);
 
-  return (
-    <div>
-      <p>Counter: {count}</p>
-      <button className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline" onClick={increaseScore}>Increase Count</button>
-    </div>
-  );
+	return (
+		<div>
+			<p>Counter: {count}</p>
+			<button
+				className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+				onClick={increaseScore}>Increase Count
+			</button>
+		</div>
+	);
 }
